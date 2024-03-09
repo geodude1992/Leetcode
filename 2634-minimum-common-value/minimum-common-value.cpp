3c3d63afe741041cff082eb7c8669af4f333ed82
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-03-08 18:01:50 -0800

    Time: 68 ms (69.61%) | Memory: 53 MB (51.54%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,5 +1,8 @@
-class Solution {
-public:
+/*  HashSet Solution
+    Time Complexity O(n + m)
+        Creating HashSet takes O(n)
+    Space Complexity O(n)
+
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         // add elements from nums 1 to hashset
         unordered_set<int> set1(nums1.begin(), nums1.end());
@@ -13,5 +16,45 @@ public:
         }
         //return -1 if there is no common element(s)
         return -1;
+    }
+
+*/
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        // Binary Search should be done on the larger array
+        // If nums1 is longer, call getCommon with the arrays swapped
+        if(nums1.size() > nums2.size()){
+            return getCommon(nums2, nums1);
+        }
+
+        // Search for each element of nums1 in nums 2
+        // Return the first common element found
+        for(int num : nums1){
+            if(binarySearch(num, nums2)){
+                return num;
+            }
+        }
+
+        // Return -1 if there are no common elements
+        return -1;
+    }
+
+private:
+    bool binarySearch(int target, vector<int>& nums){
+        int left = 0;
+        int right = nums.size() -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > target){
+                right = mid - 1;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{ // nums[mid] == target
+                return true;
+            }
+        }
+        return false;
     }
 };
