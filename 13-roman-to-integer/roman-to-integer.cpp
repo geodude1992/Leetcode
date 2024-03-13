cdf48ebded23bd2fe259f5f2582d1c3501d616b3
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-03-13 15:19:19 -0700

    Time: 21 ms (5.93%) | Memory: 13 MB (10.73%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<char, int> hmap{
             {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
         
-        for(int i = 0; i<s.size(); i++){
+        for(int i = 0; i<s.size(); ++i){
             // Case for the 1st Roman Numeral is smaller than the next
             if(hmap[s[i]] < hmap[s[i + 1]]){
                 intval -= hmap[s[i]];
