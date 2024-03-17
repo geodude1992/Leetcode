a9e1cc28aa9127689091085e4228c73f3402100e
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-03-17 01:21:53 -0700

    Time: 3 ms (80.35%) | Memory: 11.5 MB (64.07%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,14 +10,25 @@
  */
 class Solution {
 public:
+    /*
+        The idea is to use three pointers curr, prev, and forward to keep track of nodes to update reverse links.
+    Approach 1 - using Iterative Method
+        Time complexity: O(N)
+        Space complexity: O(1)
+        
+        nullptr is a keyword denoting universal null pointer value suitable for implicit convetion to any pointer type. 
+        NULL is a macro that is implementation defined and as with many macros, its text replacement may not correctly be interpreted, especially in templates. 
+        NULL may also fall into unwanted implicit convertions.
+    */
     ListNode* reverseList(ListNode* head) {
-        ListNode* current = head;
-        ListNode* previous = NULL;
-        while(current != NULL){
-            ListNode* forward = current;
-            current = current->next;
-            forward->next = previous;
-            previous = forward;
+        ListNode* current = head;   // 1
+        ListNode* previous = nullptr;
+        
+        while(current != nullptr){
+            ListNode* forward = current;    // 1, 2, 3, 4, 5
+            current = current->next;    // 2, 3, 4, 5, null
+            forward->next = previous;   // null, 1, 2, 3, 4
+            previous = forward;     // 1, 2, 3, 4, 5
         }
         return previous;
     }
