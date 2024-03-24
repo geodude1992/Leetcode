1d1ca6c0e5011f4d39a32704bbb37b83ac1c03ec
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-03-23 19:48:28 -0700

    Time: 115 ms (23.40%) | Memory: 63.7 MB (56.88%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int binarySearch(vector<int>& nums){
+        int left = 1, right = nums.size() - 1;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+
+            int count = 0;
+            for(const int& n : nums){
+                if(n <= mid) ++count;
+            }
+            if(count > mid) right = mid;
+            else 
+                left = mid + 1;
+        }
+        return left;
+    }
+public:
+    int findDuplicate(vector<int>& nums) {
+        return binarySearch(nums);
+    }
+};
