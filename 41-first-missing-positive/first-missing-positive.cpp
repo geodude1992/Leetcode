d2391b723c5c4b2e7f1d7db28306cee73829850f
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-03-25 17:33:18 -0700

    Time: 118 ms (6.57%) | Memory: 43.5 MB (81.44%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -9,11 +9,17 @@ public:
     */
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0; i < n; ++i){
-            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
-                swap(nums[i], nums[nums[i] - 1]);
-        }
         for(int i = 0; i < n; ++i)
+            // Only check +ve numbers from 0 to N and number is not in correct index
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+                // swap current index with 
+                swap(nums[i], nums[nums[i] - 1]);        
+        for(int i = 0; i < n; ++i) {
+            cout << nums[i]<<endl;
+        }   
+        for(int i = 0; i < n; ++i)
+            // Checks if the current index has a value that is in the incorrect spot
+            // return that missing value
             if(nums[i] != i + 1) 
                 return i + 1;
 
