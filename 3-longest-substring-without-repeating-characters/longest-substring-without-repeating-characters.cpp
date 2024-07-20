7f7d702880bc04ced4f12e9e92ea05bd55a8d657
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-07-20 00:11:46 -0700

    Time: 10 ms (70.47%) | Memory: 12 MB (39.71%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,6 +1,6 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
+/*
+// Set Solution
+int lengthOfLongestSubstring(string s) {
         int n = s.length();
         int maxLen = 0;
         unordered_set<char> charSet;
@@ -19,5 +19,30 @@ public:
             }
         }
         return maxLen;
+    }
+*/
+class Solution {
+public:
+    // Unordered Map Solution
+    int lengthOfLongestSubstring(string s) {
+        // improve solution by using a map instead of a set
+        int n = s.length();
+        int maxLength = 0;
+        unordered_map<char, int> charMap; // store char as keys and indices as val
+        int left = 0;
+
+        for(int right = 0; right < n; ++right){
+            // If the current character is not in the map or its index is less than left, it means it is a new unique character.
+            if(charMap.count(s[right]) == 0 || charMap[s[right]] < left){
+                charMap[s[right]] = right;
+                maxLength = max(maxLength, right - left + 1);
+            } else {
+                // If the character is repeating within the current substring, we move the left pointer to the next position after the last occurrence of the character.
+                left = charMap[s[right]] + 1;
+                charMap[s[right]] = right;
+            }
+        }
+        return maxLength;
+
     }
 };
