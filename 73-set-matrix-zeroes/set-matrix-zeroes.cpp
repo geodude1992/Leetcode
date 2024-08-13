b24ce0079f1f04d1d90a97881346d3a479a07315
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-08-13 15:45:16 -0700

    Time: 6 ms (88.22%) | Memory: 16.8 MB (57.84%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int leftCol = 1, rows = matrix.size()-1, cols = matrix[0].size()-1;
+        bool leftCol = false;
+        int rows = matrix.size()-1, cols = matrix[0].size()-1;
         
         // Use elements to set states in a top-down way
         for(int i = 0; i <= rows; i++){
-            if(matrix[i][0] == 0) leftCol = 0;
+            // Check if our left column has a zero
+            if(matrix[i][0] == 0) leftCol = true;
             for(int j = 1; j <= cols; j++){
                 if(matrix[i][j] == 0) 
                     matrix[i][0] = matrix[0][j] = 0;
@@ -18,7 +20,7 @@ public:
                 if(matrix[i][0] == 0 || matrix[0][j] == 0)
                     matrix[i][j] = 0;
             }
-            if(leftCol == 0) matrix[i][0] = 0;
+            if(leftCol == true) matrix[i][0] = 0;
         }
     }
 };
