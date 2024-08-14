97865dac44913dd11e1348d03df3e5a98e4918f9
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-08-13 21:23:32 -0700

    Time: 2 ms (58.79%) | Memory: 7.6 MB (7.91%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -4,9 +4,20 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        for(int i = 1; i <= n; i++){
+        // Linear Solution
+        /*for(int i = 1; i <= n; i++){
             if(isBadVersion(i) == true) return i;
         }
-        return false;
+        return false;*/
+
+        // Binary Search Solution O(n*log n)
+        int left = 1, right = n;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(isBadVersion(mid) == false) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
     }
 };
