4b5527692ebdca5ba63e2e9b0d50d590e0c3cf05
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-08-14 18:34:51 -0700

    Time: 64 ms (86.32%) | Memory: 85.8 MB (63.29%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
+        // keep count of $5 and $10 bills since both can be used to give back change
         int changeOf5s = 0, changeOf10s = 0;
         for(int i = 0; i <= bills.size() - 1; i++){
             if(bills[i] == 5) changeOf5s++;
+            // if customer pays with a $10 give back a $5 and 
             else if(bills[i] == 10 && changeOf5s > 0) {
                 changeOf5s--;
                 changeOf10s++;
             }
+            // 2 cases if customer pays with a $20 bill. we give back a $5 and $10 or three $5
             else if(bills[i] == 20 && changeOf5s > 0 && changeOf10s > 0){
                 changeOf5s--;
                 changeOf10s--;
             }else if(bills[i] == 20 && changeOf5s >= 3 && changeOf10s == 0){
-                changeOf5s-=3;
+                changeOf5s -= 3;
             }
 
             else return false;
