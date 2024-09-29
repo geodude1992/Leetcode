01859f882b9d9008dabd6dd9f7c08ffaad46a975
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-09-28 17:16:35 -0700

    Time: 210 ms (37.76%) | Memory: 171.3 MB (86.52%) - LeetSync

--- 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.cpp
+++ 3528-reach-end-of-array-with-max-score/reach-end-of-array-with-max-score.cpp
@@ -3,12 +3,16 @@
 
 class Solution {
 public:
-    // USE ll instead of long long int
+    // Result: you finally jump to index 'j' and convert all numbers from i to j-1 as nums[i].
+    // Answer is found out by adding the values of the array from index 0 to index array.size()-2.
+    // Notice that you can never include the last element of array in your answer.
     // T: O(n)
     // S: O(1)
     long long findMaximumScore(vector<int>& nums) {
         long long answ = 0,  n = nums.size();
+        // ll i start at index 0, ll j needs to jump at least to index 1
         for(long long i = 0, j = 1; j < n; j++){
+            // if j is at nums.size()-
             if(j == n - 1 || nums[j] > nums[i]){
                 answ += (j - i) * nums[i];
                 i = j;
