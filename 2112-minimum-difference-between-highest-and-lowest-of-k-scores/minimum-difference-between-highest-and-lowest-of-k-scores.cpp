da01e3bbb9b4d28b3ba08fefc047aeed30a8cbb0
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-09-30 20:21:26 -0700

    Time: 6 ms (94.78%) | Memory: 16.9 MB (35.59%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -5,14 +5,20 @@ public:
     // T: O(n logn) - sort takes logn, linear iteration
     // S: O(1)
     int minimumDifference(vector<int>& nums, int k) {
+        // Initialize pointers. right pointer set to k - 1 as k is <= nums.size()
         int left = 0, right = k - 1;
         sort(nums.begin(), nums.end());
+        // Set answer to INT_MAX
         int answ = INT_MAX;
+        // Iterate through window using sliding window approach
         while(right < nums.size()){
+            // Calculate the difference between the right and left element then update answer with minimum of current value and new calculated difference
             answ = min(answ, nums[right] - nums[left]);
+            // Increment both pointers
             right++;
             left++;
         }
+        // Return minimum difference
         return answ;
     }
 };
