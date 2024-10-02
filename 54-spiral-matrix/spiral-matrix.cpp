be4da46fc36f739e9011c3eec6b5867e4c70ae9a
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-10-02 12:06:12 -0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (57.98%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
+    // Set Boundaries move inward
+    // T: O(M * N) - Visited each element once
+    // S: O(1) - No extra memory is used. Resulting vector is ignored in complexity
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> result;
-        int left = 0, right = matrix[0].size() - 1;
+        // Initialize 2 Pointers top and bottom (ROWS)
         int top = 0, bottom = matrix.size() - 1;
+        // Initialize 2 Pointers left and right (COLUMNS)
+        int left = 0, right = matrix[0].size() - 1;
 
         while(top <= bottom && left <= right){
             // Traverse right when on top row
             for(int i = left; i <= right; i++){
                 result.push_back(matrix[top][i]);
             }
+            // Increment to the next row
             top++;
+
             // Traverse down when on right column
             for(int i = top; i <= bottom; i++){
                 result.push_back(matrix[i][right]);
             }
             right--;
+
             // Traverse left when on bottom row
             if(top <= bottom){
                 for(int i = right; i >= left; i--){
@@ -23,6 +31,7 @@ public:
                 }
                 bottom--;
             }
+
             // Traverse up when on the left column
             if(left <= right){
                 for(int i = bottom; i >= top; i--){
