4063a002830fc4e93df9596454ab51bfa854b73c
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-10-02 12:41:52 -0700

    Time: 12 ms (8.21%) | Memory: 9.5 MB (62.75%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -5,12 +5,12 @@ public:
         // Map unmapped characters, skip mapped characters, return false for mapped non-isomorphic characters
         // T: O(s + t)
         // S: O(n)
-        unordered_map<char, char> umap, map2;
+        unordered_map<char, char> mapStoT, mapTtoS;
         for(int i = 0; i < s.length(); ++i){
-            if(umap[s[i]] && umap[s[i]] != t[i]) return false;
-            if(map2[t[i]] && map2[t[i]] != s[i]) return false;
-            umap[s[i]] = t[i];
-            map2[t[i]] = s[i];
+            if(mapStoT[s[i]] && mapStoT[s[i]] != t[i]) return false;
+            if(mapTtoS[t[i]] && mapTtoS[t[i]] != s[i]) return false;
+            mapStoT[s[i]] = t[i];
+            mapTtoS[t[i]] = s[i];
         }
         return true;
     }
