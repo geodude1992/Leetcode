395c04293302a51f5c4dde6377310a19128f2128
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-10-03 13:45:36 -0700

    Time: 5 ms (20.34%) | Memory: 8.4 MB (44.88%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
+    // Mathy
+    // T: O(log(base 10)x)
+    // S: O(1)
     int reverse(int x) {
         long rev = 0;
         while(x != 0){ //123
+            // Store last number
             int last = x % 10; // 3, 2, 1
+            // Add last number to reverse (multiply reverse by 10 after each iteration)
             rev = (rev * 10) + last; // 3, 30 + 2, 320 + 1
+            // Reduce original number by removing the last number
             x = x / 10; // 12, 1, 0      
         }
-        return (rev > INT_MAX || rev < INT_MIN)? 0 : rev;
+        // Constraints: -2^31 <= x <= 2^(31) - 1.. return 0 if so
+        return (rev > INT_MAX || rev < INT_MIN) ? 0 : rev;
     }
 };
