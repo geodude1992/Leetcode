471a3e738e780cdc67ed3ddcd3d3ce21cae66c1d
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-10-03 20:42:15 -0700

    Time: 20 ms (38.94%) | Memory: 28.3 MB (6.66%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    // Brute force
+    /* Brute force
     // T: O(n^2) - nested loop
     // S: O(1)
     int majorityElement(vector<int>& nums) {
         int majorityCount = nums.size() / 2;
         int count;
-        int ans;
         for(int num : nums){
             count = 0;
             for(int n : nums){
@@ -15,6 +14,24 @@ public:
                 }
             }
             if(count > majorityCount) return num;
+        }
+        return -1;
+    }*/ 
+
+    // Hashmap - Allows us to count element occurrences efficiently
+    // T: O(n) - Traverse nums to add to map, traverse to find element > len(nums)/2
+    // S: O(n) - hashmap
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> counts;
+        for(int num : nums){
+            if(counts.find(num) == counts.end()){
+                counts[num] = 1;
+            }else{
+                counts[num] += 1;
+            }
+            for(auto itr : counts){
+                if(itr.second > nums.size() / 2) return itr.first;
+            }
         }
         return -1;
     }
