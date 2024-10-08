625f3f21cd273b7a29cb16638734eda7acc5814e
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-10-08 13:02:23 -0700

    Time: 88 ms (49.32%) | Memory: 75.9 MB (73.05%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -7,13 +7,14 @@ public:
         int noOfOperations = target[0];
         // Loop through the target array starting from the second element and compare with previous element
         for(int i = 1; i < target.size(); i++){
-        // If the current element is greater than the previous we have an increasin segment
-            if(target[i] >= target[i-1]){
-        // Calculate the difference and add this difference to the noOfOperations
+            // If the current element is greater than the previous we have an increasing segment
+            if(target[i] > target[i-1]){
+                // Calculate the difference and add this difference to the noOfOperations
                 noOfOperations += (target[i]-target[i-1]);
-        // Repeat process for all segments 
             }
+            // Repeat process for all segments 
         }
+        // After iterating through the target array, the value of noOfOperations will represent the minimum number of operations required to transform the initial array into the target array, considering only the increasing segments.
         return noOfOperations;
     }
 };
