0f7506cc661e6afaeb56f498b9b773fad8cbd258
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-10-10 19:11:09 -0700

    Time: 18 ms (60.51%) | Memory: 19.2 MB (8.55%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.cpp
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.cpp
@@ -1,21 +1,30 @@
+// Queue Solution
+// T: O(1) - No loop in next(val) function
+// S: O(N) - N is the size of queue
 class MovingAverage {
     /** Queue - Pop from front when size == queue size (window) and add from rear end */
     double windowSum = 0;
-    int window;
+    int windowSize;
     queue<double> numQueue;
 
 public:
     MovingAverage(int size) {
-        window = size;
+        windowSize = size;
     }
     
     double next(int val) {
-        if(numQueue.size() == window){
+        // 4. Check if our queue is full
+        if(numQueue.size() == windowSize){
+            // 5. Subract front element of queue from our window sum
             windowSum -= numQueue.front();
+            // 6. Pop from front of queue
             numQueue.pop();
         }
+        // 1. First we push the input value in the queue
         numQueue.push(val);
+        // 2. Update the window sum
         windowSum += val;
+        // 3. Return the Average(sum/size)
         return windowSum / numQueue.size();
     }
 };
