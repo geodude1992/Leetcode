8ebcbc79cd746ca3ff10f788d29b758b5eebef90
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-10-15 22:45:41 -0700

    Time: 0 ms (100.00%) | Memory: 22.4 MB (69.29%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.cpp
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -3,13 +3,14 @@ public:
     // T: O(1)
     // S: O(1)
     string triangleType(vector<int>& nums) {
-        // Check if triangle is equilateral
+        // Check if triangle is Equilateral
         if(nums[0] == nums[1] && nums[1] == nums[2]) return "equilateral";
-        // Check if triangle is equilateral
+        // Check if triangle is Isosceles - ANY two lengths of equal size AND their sum is greater than the third length
         else if((nums[0] == nums[1] && (nums[0] + nums[1] > nums[2])) || 
                 (nums[1] == nums[2] && (nums[1] + nums[2] > nums[0])) || 
                 (nums[0] == nums[2] && (nums[0] + nums[2] > nums[1])))
             return "isosceles";
+        // Check if triangle is Scalene - sum of ALL two lengths is greater than the third length
         else if((nums[0] + nums[1] > nums[2]) && (nums[1] + nums[2] > nums[0]) && (nums[0] + nums[2] > nums[1]))
             return "scalene";
 
