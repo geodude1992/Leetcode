b18daa386e8498a86097f29c46c1e9f630e4fe9e
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2024-10-16 21:21:48 -0700

    Time: 25 ms (72.95%) | Memory: 26.2 MB (10.92%) - LeetSync

--- 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
@@ -1,6 +1,7 @@
 class CustomStack {
 public:
     // Arrays with Lazy Propagation, a technique where updates are delayed until absolutely necessary.
+    // T: O(1) push, pop, inc | S: O(2 * maxSize) == O(maxSize)
     vector<int> stacc;
     vector<int> increments; // lazy propagation
     // Current top index of the stack
@@ -19,6 +20,8 @@ public:
         }
     }
     
+    // When popping an element, we return the value at the top of the stack, including any increments that apply to it.
+    // This is where lazy propagation is used.
     int pop() {
         // Check if stack is empty if so return -1
         if(topIndex < 0){
@@ -27,17 +30,20 @@ public:
         // Calculate the actual value with increment
         int result = stacc[topIndex] + increments[topIndex];
 
-        // Propagate the increment to the element below
+        // Propagate the increment to the element below the element being popped
+        // Preserving the necessary increments for future pops.
         if(topIndex > 0) increments[topIndex - 1] += increments[topIndex];
 
         // Reset the increment for this position
         increments[topIndex] = 0;
         
+        // Then, we decrement topIndex to remove the current top element.
         topIndex--;
 
         return result;
     }
     
+    // Instead of directly modifying the bottom k elements, we simply update the value at index k-1 in incrementArray
     void increment(int k, int val) {
         if(topIndex >= 0){
             int incrementIdx = min(topIndex, k - 1);
