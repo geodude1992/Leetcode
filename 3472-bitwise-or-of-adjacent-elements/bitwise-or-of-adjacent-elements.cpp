30f7173080d27d92aea55318f00558065dfab848
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-01-19 19:18:39 -0800

    Time: 0 ms (100.00%) | Memory: 42 MB (30.61%) - LeetSync

--- 3472-bitwise-or-of-adjacent-elements/bitwise-or-of-adjacent-elements.cpp
+++ 3472-bitwise-or-of-adjacent-elements/bitwise-or-of-adjacent-elements.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
+    /* 
+    nums = [1,3,5] = [0001, 0011, 0101]
+    1 => 0001 
+    3 => 0011
+    res = [0011, ...]
+    3 => 0011
+    5 => 0101
+    res = [0011, 0111]
+
+    T: O(n)
+    */
     vector<int> orArray(vector<int>& nums) {
         vector<int> result;
         
-        for(int i = 1; i < nums.size(); ++i){
-            result.push_back(nums[i] | nums[i - 1]);
+        // Range (O, n-2)
+        for(int i = 0; i < nums.size()-1; ++i){
+            result.push_back(nums[i] | nums[i + 1]);
         }
 
         return result;
