9debc762eed55835d1c9743a0f70a874de4391df
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-04-02 02:05:27 -0700

    Time: 8 ms (10.95%) | Memory: 14.1 MB (21.05%) - LeetSync

--- 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.cpp
+++ 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    // Sort with Max Heap Solution
+    // T: O(m log m) | m rows
+    // S: O(n) pq
     int deleteGreatestValue(vector<vector<int>>& grid) {
-        //priority_queue<int> pq;
         int sumOfMaxs = 0;
 
-        // Sort
-        for(auto &x : grid)
-            sort(x.rbegin(), x.rend());
+        // 1. Sort each row of the grid.
+        for(auto &row : grid)
+            sort(row.begin(), row.end());
         
-        for(int i = 0; i < grid[0].size(); i++){
+        // 2. for each column, use a priority queue to find the largest value in that column.
+        for(int col = 0; col < grid[0].size(); col++){
             priority_queue<int> pq;
-            for(int j = 0; j < grid.size(); j++){
-                pq.push(grid[j][i]);
+            for(int row = 0; row < grid.size(); row++){
+                pq.push(grid[row][col]);
             }
+            // 3. Add these largest values together to get the final answer.
             sumOfMaxs += pq.top();
         }
 
