762647378ea9758e7c310ee61ff6fab4043fbe0a
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-04-03 16:44:34 -0700

    Time: 2 ms (51.47%) | Memory: 13.3 MB (33.02%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -2,15 +2,20 @@
 
 class Solution {
 public:
+    // Max Heap Solution
+    // T: O(n * logn + k) - initialize k cost n, populating the heap cost logn, k iterations
+    // S: O(n) - pq is not used in result
     long long pickGifts(vector<int>& gifts, int k) {
         // Sort numbers in descending order
         priority_queue<int> maxHeap(gifts.begin(), gifts.end());
         long long giftsLeft = 0;
 
+        // Get Sq Root of each Max element for K seconds and push element back to Heap
         for(int i = 0; i < k; ++i){
             int sqroot = sqrt(maxHeap.top()); maxHeap.pop();
             maxHeap.push(sqroot);
         }
+        // Since the size of pq remains the same, we iterate through length of gifts array and add to sum
         for(int i = 0; i < gifts.size(); ++i){
             giftsLeft += maxHeap.top(); maxHeap.pop();
         }
