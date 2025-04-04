cf86e0327c73a7a343b7d551f627e234eee396bb
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-04-04 12:01:25 -0700

    Time: 0 ms (100.00%) | Memory: 8.5 MB (48.13%) - LeetSync

--- 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.cpp
+++ 2327-largest-number-after-digit-swaps-by-parity/largest-number-after-digit-swaps-by-parity.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int largestInteger(int num) {
-        priority_queue<int> evenHeap;
-        priority_queue<int> oddHeap;
-        string nums = to_string(num);
+        priority_queue<int> evenHeap;   // priority queue to store EVEN digits in descending order
+        priority_queue<int> oddHeap;    // priority queue to store ODD digits in descending order
+        string nums = to_string(num);   // converting num to a string for easy access of the digits
         int ans = 0;
 
         for(int i = 0; i < nums.size(); ++i){
             int digit = nums[i] - '0';
-            if(digit % 2) evenHeap.push(digit);
+            if(digit % 2 == 0) evenHeap.push(digit);
             else oddHeap.push(digit);
         }
 
         for(int i = 0; i < nums.size(); ++i){
             ans = ans * 10;
-            if((nums[i] - '0') % 2) {
+            if((nums[i] - '0') % 2 == 0) {
                 ans += evenHeap.top(); evenHeap.pop();
             } else {
                 ans += oddHeap.top(); oddHeap.pop();
