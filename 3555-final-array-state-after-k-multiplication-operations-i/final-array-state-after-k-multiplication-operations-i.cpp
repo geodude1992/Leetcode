40c04e4b3b6c660b2f6c45795d6b7dea70a4d72d
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-04-10 21:15:06 -0700

    Time: 1 ms (37.77%) | Memory: 27.1 MB (87.11%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,9 +1,12 @@
 #include <algorithm>
 class Solution {
 public:
+    // min_element Solution
+    // T: O(K * N)
     // The std::min_element() in C++ is an STL algorithm that is used to find the minimum element in a given range. 
     // This range can be array, vector, list or any other container. It is defined inside the <algorithm> header file
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        // 1. Iterate through the array k times
         for(int i = 0; i < k; ++i){
             // std::min_element(first, last, comp);
             /*
@@ -12,7 +15,9 @@ public:
             comp: Binary function, functor or lambda expression that compares two elements in the range. 
             By default, it is set as < operator.
             */
+            // 2. Find the index of the minimum element in the current array
             auto minItr = std::min_element(nums.begin(), nums.end());
+            // 3. Multiply the minimum element by the given multiplier
             *minItr *= multiplier;
         }
         return nums;
