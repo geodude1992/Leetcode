0eca7894d76dfa4ab11e40a4adbe555803ca83d3
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-04-26 12:56:21 -0700

    Time: 50 ms (69.70%) | Memory: 14.4 MB (45.97%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     // DP Solution O(n^2)
     int lengthOfLIS(vector<int>& nums) {
+        // dp[i] represents the length of the longest increasing subsequence that ends with the ith element.
         vector<int> dp(nums.size(), 1);
 
+        // Starting at index 1 check all the index before it
         for(int i = 1; i < nums.size(); i++){
             for(int j = 0; j < i; j++){
                 // valuate its in increasing order
                 if(nums[i] > nums[j]) dp[i] = max(dp[i], 1 + dp[j]);
             }
         }
-        int lis = 0;
-        for(int c : dp){
-            lis = max(lis, c);
-        }
-        return lis;
+        /*int lis = 0;
+        for(int n : dp){
+            lis = max(lis, n);
+        }*/
+        return *std::max_element(dp.begin(), dp.end());
     }
 };
