2d26ffe2be5165fee4d1a8dafef78d1402eaa3fc
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-04-27 15:53:20 -0700

    Time: 30 ms (38.63%) | Memory: 16.3 MB (65.96%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -5,17 +5,17 @@ public:
         if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
             return;
         // return if current position is of water=='0' or is already visited=='2'
-        if(grid[i][j] == '2' || grid[i][j] == '0')
+        if(grid[i][j] == '0')
             return;
         
         // mark the current as visited
-        grid[i][j] = '2';
+        grid[i][j] = '0';
 
         // do DFS in all 4 directions
-        DFS(grid, i+1, j);    // RIGHT
-        DFS(grid, i, j+1);    // DOWN
-        DFS(grid, i-1, j);    // LEFT
-        DFS(grid, i, j-1);    // UP
+        DFS(grid, i+1, j);    // DOWN
+        DFS(grid, i, j+1);    // RIGHT
+        DFS(grid, i-1, j);    // UP
+        DFS(grid, i, j-1);    // LEFT
     }
 
     int numIslands(vector<vector<char>>& grid) {
