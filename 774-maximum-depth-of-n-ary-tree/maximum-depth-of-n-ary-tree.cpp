137923c9f48803d412cdd8fa52ed05ecad12cd9a
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-05-06 11:56:30 -0700

    Time: 8 ms (89.78%) | Memory: 14.5 MB (73.38%) - LeetSync

---
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    /* DFS Solution
+    int maxDepth(Node* root) {
+        if(!root) return 0;
+        int mDepth = 0;
+        for(auto child : root->children){
+            mDepth = max(mDepth, maxDepth(child));
+        }
+        return 1 + mDepth;
+    }*/
+
+    // BFS Solution
+    int maxDepth(Node* root) {
+        if(!root) return 0;
+        int mDepth = 0;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            mDepth++;
+            int depthNodes = q.size();
+            for(int i = 0; i < depthNodes; ++i){
+                auto node = q.front(); q.pop();
+                for(auto child : node->children) {
+                    if(child) q.push(child);
+                }
+            }
+        }
+        return mDepth;
+    }
+};
