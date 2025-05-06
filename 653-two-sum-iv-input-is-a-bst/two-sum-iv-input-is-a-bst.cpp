ede0a51745c6ed6e26b9f3019ff870d284ea954c
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-05-05 21:54:25 -0700

    Time: 0 ms (100.00%) | Memory: 37.3 MB (59.65%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> trav;
+        inorder(root, trav);
+        int l = 0, r = trav.size()-1;
+        while(l < r){
+            int sum = trav[l] + trav[r];
+            if(sum == k) return true;
+            if(sum < k) l++;
+            else r--;
+        }
+        return false;
+    }
+
+    // INORDER Traversal Left=>Root=>Right
+    void inorder(TreeNode* node, vector<int>& v){
+        if(!node) return;
+        inorder(node->left, v); // Visited Left
+        v.push_back(node->val);
+        inorder(node->right, v);
+    }
+};
