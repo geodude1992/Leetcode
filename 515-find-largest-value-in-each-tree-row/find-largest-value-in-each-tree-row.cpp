c780b3707bb7d4dc6fbe77c8275c97590490e270
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-05-09 13:12:53 -0700

    Time: 0 ms (100.00%) | Memory: 22.9 MB (34.54%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -11,15 +11,16 @@
  */
 class Solution {
 public:
+    // BFS Solution
     vector<int> largestValues(TreeNode* root) {
-        if(!root) return {};
+        if(!root) return {};    // Edge Case: Empty Tree
 
         vector<int> values;
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
             int qSize = q.size();
-            int maxVal = INT_MIN;  
+            int maxVal = INT_MIN;  // Constraint: -2^31 <= Node.val <= 2^31 - 1
             for(int i = 0; i < qSize; ++i){ 
                 auto node = q.front(); q.pop();
                 maxVal = max(node->val, maxVal);            
