5b0410fa5afd4885ab0e8045232a0bbfbb0f1ad2
Jovanni Garcia <72895219+geodude1992@users.noreply.github.com>
2025-06-11 12:11:12 -0700

    Time: 4 ms (1.66%) | Memory: 14.8 MB (74.58%) - LeetSync

---
+++ 86-partition-list/partition-list.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // Two List Solution
+    ListNode* partition(ListNode* head, int x) {
+        ListNode before_head(0), after_head(0); // Create two Node objects
+        ListNode* pBefore = &before_head, * pAfter = &after_head; // create two pointers to node objects
+
+        // Iterate the original LL using head
+        while(head){
+            // If the node's value pointed to by head is LESSER than x, the node should be part of Before List
+            if(head->val < x) pBefore = pBefore->next = head; 
+            // Else the node should be part of After List
+            else pAfter = pAfter->next = head;
+
+            head = head->next;
+        }
+        // Combine both List, before and after
+        pBefore->next = after_head.next;
+        pAfter->next = nullptr;
+
+        // use dot notation is b_head is an object
+        return before_head.next;
+    }
+};
